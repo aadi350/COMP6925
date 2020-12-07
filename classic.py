cf66ccae6b2861678bfac0dddda0edccb433e091
aadi350 <a.aadi380@gmail.com>
2020-12-07 14:40:58 -0400

    Refactor into function

--- classic.py
+++ classic.py
@@ -1,37 +1,37 @@
 import pulp as pl
-from pulp import LpMinimize, LpProblem, lpSum, LpVariable, LpBinary
+from pulp import LpMinimize, LpProblem, lpSum, LpVariable, LpBinary, LpStatus
 
 from app import load_tasks
 
-print(pl.listSolvers())
-TIME_MAX = 300
+WINDOW = 300
 HIGH = 100
 LOW = 70
-tasks = load_tasks()[350]
-task_matrix = []
-
-model = LpProblem(name='MaxPriority', sense=LpMinimize)
-
-high_f = {i: LpVariable(name=f"high_{i}", lowBound=0, cat=LpBinary) for i in range(len(tasks))}
-low_f = {i: LpVariable(name=f"low_{i}", lowBound=0, cat=LpBinary) for i in range(len(tasks))}
 
 
-# single processor constraint
-for i in range(len(tasks)):
-    model.add(high_f[i] + low_f[i] == 1)
+def optimize_single_classic(task_list):
+    model = LpProblem(name='MaxPriority', sense=LpMinimize)
 
-model.add(lpSum((tasks[i].cycles/HIGH)*high_f[i] + (tasks[i].cycles/LOW)*low_f[i] for i in range(len(tasks))) <= TIME_MAX)
+    high_f = {i: LpVariable(name=f"high_{i}", lowBound=0, cat=LpBinary) for i in range(len(task_list))}
+    low_f = {i: LpVariable(name=f"low_{i}", lowBound=0, cat=LpBinary) for i in range(len(task_list))}
 
-# # Maximize priority
-# model += lpSum(tasks[i].priority*(high_f[i] + low_f[i]) for i in range(len(tasks)))
+    # single processor constraint
+    for i in range(len(task_list)):
+        model.add(high_f[i] + low_f[i] == 1)
 
-# Minimize power
-model += lpSum(tasks[i].cycles*high_f[i] + tasks[i].cycles*low_f[i] for i in range(len(tasks)))
+    model.add(lpSum(
+        (task_list[i].cycles / HIGH) * high_f[i] + (task_list[i].cycles / LOW) * low_f[i] for i in range(len(task_list))) <= WINDOW)
 
-status = model.solve()
+    # # Maximize priority
+    # model += lpSum(tasks[i].priority*(high_f[i] + low_f[i]) for i in range(len(tasks)))
 
-print('Net time', lpSum((tasks[i].cycles/HIGH)*high_f[i].value() + (tasks[i].cycles/LOW)*low_f[i].value() for i in range(len(tasks))))
-print('Net power', lpSum(HIGH*high_f[i].value() + LOW*low_f[i].value() for i in range(len(tasks))))
+    # Minimize power
+    model += lpSum(task_list[i].cycles * high_f[i] + task_list[i].cycles * low_f[i] for i in range(len(task_list)))
 
-# for i in range(len(tasks)):
-#     print(high_f[i].value(),' ',low_f[i].value() )
+    status = LpStatus[model.solve()]
+    best_time = lpSum((task_list[i].cycles / HIGH) * high_f[i].value() + (task_list[i].cycles / LOW) * low_f[i].value() for i in range(len(task_list)))
+    best_power = lpSum(HIGH*high_f[i].value() + LOW*low_f[i].value() for i in range(len(task_list)))
+
+    # for i in range(len(tasks)):
+    #     print(high_f[i].value(),' ',low_f[i].value() )
+
+    return status, best_time, best_power, len(task_list), None
